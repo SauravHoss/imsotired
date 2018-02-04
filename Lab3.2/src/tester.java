7b3f0498ebaf01558f80ebe8f0ebccb6de83fc7a
Rome <Rome@Rome>
2018-02-04 12:05:03 -0500

    hg

--- Lab3.2/src/tester.java
+++ Lab3.2/src/tester.java
@@ -4,7 +4,36 @@ public class tester
 
 	public static void main(String[] args) 
 	{
-
+		int [] test1 = {1, 4, 4, 5, 2, 4, 3, 17, 0};
+		
 	}
 
+		public static void merge() 
+		{
+		
+		}
+		
+		public static void merger() 
+		{
+			
+		}
+	
+		public static void quick() 
+		{
+		
+		}
+	
+		public static void partitioner() 
+		{
+			
+		}
+		
+	//Swaps two items in an array of integers
+		public static void swapperi(int [] arr, int x, int y) 
+		{
+			int holder = arr[x];
+			arr[x] = arr[y];
+			arr[y] = holder;	
+		}
+		
 }
