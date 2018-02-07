974845dd875b103a9fd68d8a2cceb85ac951512c
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2018-02-07 09:23:20 -0500

    aa

--- Lab3.2/src/tester.java
+++ Lab3.2/src/tester.java
@@ -4,22 +4,22 @@ public class tester
 
 	public static void main(String[] args) 
 	{
-		String [] list1 = { "ab","bc" };
-		String [] list2 = { "bc", "ab"};
+		String [] list1 = { "b","ab", "cc" };
+		String [] list2 = { "aa", "a", "v"};
 		merge(list1,list2);
 	}
 
-	public static String [] merge(String [] list1, String [] list2) 
+	public static void merge(String [] list1, String [] list2) 
 	{
 		//Precondition : Both lists are sorted
 	
-		String [] full = new String [list1.length+list2.length-2];
+		String [] full = new String [(list1.length) + (list2.length)];
 		
 		int j = 0;
 		int l = 0;
 		int r = 0;
 		
-		while(j < full.length) 
+		while(j < full.length-1) 
 		{
 			if (list1[l].compareTo(list2[r]) > 0)
 			{
@@ -35,14 +35,19 @@ public class tester
 				l++;
 			}
 		}
-		return full;
+		//return full;
+		System.out.print(java.util.Arrays.toString(full));
 	}
 	
-	//public static int partition(int [] list) 
+	public static int partition(int [] list) 
 	{
+		int [] temp = new int [list.length];		
+		
+		int base = list[0];
+		
 		
 	}
-
-		
+	
+	
 	
 }
