16a9a1b28a8f122d6d70f95619d7060fb4f91fd9
Rome <Rome@Rome>
2018-02-07 21:44:57 -0500

    f

--- Lab3.2/src/tester.java
+++ Lab3.2/src/tester.java
@@ -5,8 +5,11 @@ public class tester
 	public static void main(String[] args) 
 	{
 		String [] list1 = { "b","ab", "cc" };
-		String [] list2 = { "aa", "a", "v"};
+		String [] list2 = { "va", "a", "v"};
 		merge(list1,list2);
+		
+		int [] listy = {1,9,5,2,5,6};
+	//	partition(listy, 1, 6);
 	}
 
 	public static void merge(String [] list1, String [] list2) 
@@ -19,7 +22,7 @@ public class tester
 		int l = 0;
 		int r = 0;
 		
-		while(j < full.length-1) 
+		while(l < list1.length && r < list2.length) 
 		{
 			if (list1[l].compareTo(list2[r]) > 0)
 			{
@@ -39,14 +42,33 @@ public class tester
 		System.out.print(java.util.Arrays.toString(full));
 	}
 	
-	public static int partition(int [] list) 
-	{
-		int [] temp = new int [list.length];		
-		
-		int base = list[0];
-		
-		
-	}
+	 public static int partition( int [] arr, int low, int high)
+	 {
+		   int pivot = arr[0];  
+		 
+		    int i = (low - 1);
+
+		    for (int j = low; j <= high- 1; j++)
+		    {
+		       
+		        if (arr[j] <= pivot)
+		        {
+		            i++; 
+		            swapperi(arr, i, j);
+		        }
+		    }
+		    swapperi(arr, i+1, high);
+		    return (i + 1);
+		}
+
+	
+	//Swaps two items in an array of integers
+		public static void swapperi(int [] arr, int x, int y) 
+		{
+			int holder = arr[x];
+			arr[x] = arr[y];
+			arr[y] = holder;	
+		}
 	
 	
 	
